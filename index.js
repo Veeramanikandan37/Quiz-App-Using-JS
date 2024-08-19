896246d898b19a6054302319191a6af9c5b3af14
Veeramanikandan37 <163031759+Veeramanikandan37@users.noreply.github.com>
2024-08-19 08:38:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// function correct(){
+//     var val=document.getElementById('optiona');
+//     val.style=("background-color:green; color:white;");
+// }
+// function incorrect1(){
+//     var val=document.getElementById('optionb');
+//     val.style=("background-color:red; color:white;");
+// }
+// function incorrect2(){
+//     var val=document.getElementById('optionc');
+//     val.style=("background-color:red; color:white;");
+// }
+// function incorrect3(){
+//     var val=document.getElementById('optiond');
+//     val.style=("background-color:red; color:white;");
+// }
+
+function correct(element){
+    element.style.backgroundColor="green";
+    element.style.color="white";
+}
+function incorrect(element){
+    element.style.backgroundColor="red";
+    element.style.color="white";
+}
